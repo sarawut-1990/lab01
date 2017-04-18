63545697dde397a60d0bee11095b3b6d9081c538
sarawut-1990 <sarawutturbo@gmail.com>
2017-04-18 15:03:17 +0700

    first Lab

---
+++ src/main/java/rmuti/lab1/App.java
@@ -0,0 +1,20 @@
+package rmuti.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 ArrayOperation obj = new ArrayOperation();
+         
+         int sumObj = obj.SumArrays();
+         System.out.println("Sum value is "+sumObj);
+         
+         int maxObj = obj.maxArrays();
+         System.out.println("Max value is "+maxObj);
+
+    }
+}
